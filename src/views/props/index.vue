6d4745bcf8bec103f5d731f024a80d1eacd6ef7c
huatten <nbilly@163.com>
2023-08-03 11:47:53 +0800

    feat: 🚀 add eslint props check demo

---
+++ src/views/props/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="custom-component"></div>
+</template>
+<script setup>
+const props = defineProps({
+  element: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+// eslint-disable-next-line vue/no-setup-props-destructure
+console.log(props.element)
+</script>

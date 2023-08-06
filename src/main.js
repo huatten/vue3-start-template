9ae403ae051f3883aab67b434762cb36d9fe1e80
jinhua5 <nbilly@163.com>
2023-08-06 13:36:15 +0800

    feat: 🚀 注册directives

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import 'virtual:svg-icons-register'
 import App from './App.vue'
 import routers from './routers'
 import stores from './stores'
+import directives from './directives'
 import '@/styles/index.scss'
 
 const app = createApp(App)
 
-app.use(routers).use(stores).mount('#app')
+app.use(routers).use(stores).use(directives).mount('#app')

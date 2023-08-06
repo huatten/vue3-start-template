0c82429c65d39aa91aa47ba8d49c0351bc017386
jinhua5 <nbilly@163.com>
2023-08-06 13:32:58 +0800

    chore: 🔨 移除tailwind

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'virtual:svg-icons-register'
 import App from './App.vue'
 import routers from './routers'
 import stores from './stores'
-// import '@/styles/tailwind.css'
 import '@/styles/index.scss'
 
 const app = createApp(App)

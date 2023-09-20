f36db55dc8983c127dd68fb95c18cc2818944608
huatten <nbilly@163.com>
2023-09-20 19:49:19 +0800

    feat: 🚀 定义transition.scss

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,3 @@
     </transition>
   </router-view>
 </template>
-
-<style lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
